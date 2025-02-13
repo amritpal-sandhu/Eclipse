bc55896728816b83058fc163e49d241aac7e024a
Sukhpal0 <ssandhutext@gmail.com>
2025-02-13 11:27:45 -0500

    loops

---
+++ forloop.java
@@ -0,0 +1,37 @@
+public class forloop{
+    public static void main(String[] args){
+
+        //  for(int i = 0; i < 11; i++){
+        //     System.out.println(i);
+    
+        // int i=0;
+        // while(i<11){
+        //     System.out.println(i);
+        //     i=i+1;
+
+        // do{
+        //     System.err.println(i);
+        //     i=i+1;
+        // }while (i < 11);
+
+        // the main differance between  whileloop and do whileloops is that while loop checks the 
+        // condition first than start the condition, whereas, do whileloop exicute the condition first 
+        // and than checks it. Another differance is that, there is excution at the end of while in the 
+        // do while loop, not in the whileloop.
+
+        // An Example for while and Do while loop
+
+       int  j =12;
+        while(j <11){
+            System.err.println("It will not exicute");
+        }
+
+        do{
+            System.err.println("It will exicute"); 
+        }while(j < 11);
+
+
+
+    }
+    
+}
