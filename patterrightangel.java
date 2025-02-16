11411fefb3ad9b47038bd735a3cb1b3aacae8831
Sukhpal0 <ssandhutext@gmail.com>
2025-02-16 15:49:42 -0500

    ques on pattern

---
+++ patterrightangel.java
@@ -0,0 +1,26 @@
+package loops;
+
+public class patterrightangel {
+    public static void main(String[] args) {
+
+        // printing right angled star 
+        for(int i=1;i<=4;i++){
+            for (int j=1;j<=i;j++) {
+                System.out.print("*");
+            }System.out.println(" ");
+        } 
+
+        // inverted half Pyramid
+        // for(int k=1;k<=5;k++){ 
+        //     for (int l=5;l>=k;l--) {         // changed inner loop 
+        //         System.out.print("*");
+        //     }System.out.println(" ");
+        // } 
+
+             for(int k=4;k>=1;k--){             // here outer.
+                for (int l=1;l<=k;l++) {
+                    System.out.print("*");
+                }System.out.println(" ");
+            } 
+    }
+}
