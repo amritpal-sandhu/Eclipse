9ee48f6f4240eaa25461c24113e3d386c47a7fae
Sukhpal0 <ssandhutext@gmail.com>
2025-02-19 17:52:09 -0500

    made some change in the file

--- forloopeg.java
+++ forloopeg.java
@@ -4,16 +4,16 @@ import java.util.*;
 public class forloopeg {
     public static void main(String[] args) {
         // sum pf n prime numbers
-        Scanner sc = new Scanner(System.in);
+        Scanner esc = new Scanner(System.in);
         System.err.println("Enter you prime no, to get it's sum");
-        int num=sc.nextInt();
+        int num=esc.nextInt();
 
         int n=0;
         for(int i=1; i <= num; i++){
             n = n+i;
         }
         System.err.println(n); 
-        sc.close();
+        esc.close();
     }
     
 }
